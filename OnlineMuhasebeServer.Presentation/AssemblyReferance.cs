9707fd641cf7d99566dcbce687197928b2bf66f1
Şeymanur Çürükcü <seymanurcurukcu@gmail.com>
2023-10-30 13:52:30 +0300

    Proje dosyası ekle.

---
+++ OnlineMuhasebeServer.Presentation/AssemblyReferance.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Reflection;
+
+namespace OnlineMuhasebeServer.Presentation
+{
+    public static class AssemblyReferance
+    {
+        public static readonly Assembly Assembly = typeof(Assembly).Assembly;   
+    }
+}
---
+++ OnlineMuhasebeServer.Presentation/Controller/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Presentation.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Presentation.Controller
+{
+    public sealed class TestController :ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("işlem başarılı");
+        }
+    }
+}
